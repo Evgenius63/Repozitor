78057321fbc5b97fc59135270ab6db45beb1ad30
Evgeniy <pravo63@yandex.ru>
2025-06-04 20:25:40 +0400

    Начало разработки

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+	
+	<?php
+
+	set_include_path(get_include_path() . PATH_SEPARATOR . "/path/to/dompdf");
+
+require_once "dompdf_config.inc.php";
+
+$dompdf = new DOMPDF();
+
+$html = <<<'ENDHTML'
+<html>
+ <body>
+  <h1>Hello Dompdf</h1>
+ </body>
+</html>
+ENDHTML;
+
+$dompdf->load_html($html);
+$dompdf->render();
+
+$dompdf->stream("hello.pdf");
+?>
+		
+</body>
+</html>
